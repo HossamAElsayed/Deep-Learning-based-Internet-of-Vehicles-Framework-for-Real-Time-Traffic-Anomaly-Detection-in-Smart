48232d82236df3c7f577bc50415cd202de3a1499
HossamAElsayed <33600370+HossamAElsayed@users.noreply.github.com>
2022-07-12 20:17:45 +0200

    Add files via upload

---
+++ Backend/main.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.path.insert(0, '/home/pi/Documents/DB/')
+
+from DB.modify import *
+
+# add_new_record('Test from code by me!') 
+#search_record('id', 1)
+
+#search_record('id', 1)
+if __name__ == "__main__":
+    print("program started ... ")
+    # establish_connection_with_main_server()
+

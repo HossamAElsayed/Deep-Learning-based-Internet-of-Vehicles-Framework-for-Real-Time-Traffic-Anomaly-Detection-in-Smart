ab27e0c2961cbf618dc23952e0fc076dd6523480
HossamAElsayed <33600370+HossamAElsayed@users.noreply.github.com>
2022-07-15 01:00:48 +0200

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,14 +1,15 @@
 import sys
+import threading
+
+from bluetooth import *
+from BT.bluetooth import bt_init
+from MQTT.mqtt_subscribe import run_mqtt_server
 
 sys.path.insert(0, '/home/pi/Documents/DB/')
 
 from DB.modify import *
 
-# add_new_record('Test from code by me!') 
-#search_record('id', 1)
-
-#search_record('id', 1)
+# Main Driver 
 if __name__ == "__main__":
-    print("program started ... ")
-    # establish_connection_with_main_server()
-
+    print("OBU is Online ... ")
+    run_mqtt_server()

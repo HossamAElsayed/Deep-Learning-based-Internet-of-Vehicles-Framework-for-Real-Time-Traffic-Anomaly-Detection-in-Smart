ade9d05a7b68789cbf210ac5b9e15c0b7500774d
HossamAElsayed <33600370+HossamAElsayed@users.noreply.github.com>
2022-07-14 23:54:04 +0100

    Create camera.py

---
+++ Backend/CAM/camera.py
@@ -0,0 +1,13 @@
+from picamera2 import Picamera2, Preview
+import time
+
+print("Camera Started...")
+
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
+#picam2.capture_file("test.jpg")
+
